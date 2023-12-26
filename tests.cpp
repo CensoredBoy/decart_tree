e690daf3f7901134768e8c9ad337216fd25ad91f
Илья Волочинский ТКИ-442 <117316669+DikijOniRonin@users.noreply.github.com>
2023-12-26 23:51:26 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -2,13 +2,13 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-TESTINSERT(module_name, test_name) {
+TEST(module_name, test_name1) {
 	DTree t = DTree(1);
 	t.insert(121);
     // Google Test will also provide macros for assertions.
     ASSERT_EQ(t.find(121), true);
 }
-TESTREMOVE(module_name, test_name) {
+TEST(module_name, test_name2) {
 	DTree t = DTree(1);
 	t.insert(121);
 	t.remove(121);
@@ -16,19 +16,19 @@ TESTREMOVE(module_name, test_name) {
     ASSERT_EQ(t.find(121), false);
 }
 
-TESTNODE(module_name, test_name) {
+TEST(module_name, test_name3) {
 	Node t = Node(1);
     // Google Test will also provide macros for assertions.
     ASSERT_EQ(Nodes::find(1), true);
 }
 
-TESTTESTNODEKEY(module_name, test_name) {
+TEST(module_name, test_name4) {
 	Node t = Node(1);
     // Google Test will also provide macros for assertions.
     ASSERT_EQ(t.key, 1);
 }
 
-TESTNODEPRIOR(module_name, test_name) {
+TEST(module_name, test_name5) {
 	Node t = Node(1);
     // Google Test will also provide macros for assertions.
     ASSERT_EQ(t.prior, 1);
